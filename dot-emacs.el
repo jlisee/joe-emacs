c1ee14a3e6fa21fd4f628d477abf885747036d27
Joseph Lisee <jlisee@gmail.com>
2011-06-13 09:17:43 -0400

    Added recompile shortcut key

--- dot-emacs.el
+++ dot-emacs.el
@@ -93,6 +93,7 @@
 (global-set-key "\C-cc" 'comment-region)
 (global-set-key "\C-cu" 'uncomment-region)
 (global-set-key "\C-ca" 'ack)
+(define-key global-map [f7] 'recompile) ; recompile
 
 ;; (define-key global-map "\C-xw" 'what-line)
 ;; (define-key global-map "\C-z" 'undo)
